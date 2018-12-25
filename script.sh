f07b33dcfcd4c6d43263661e687927f5fdeca3a5
Dimitri BERGER <hylow423@gmail.com>
2018-12-25 12:44:34 +0100

    init core apps zsh,git,hyper + theming

---
+++ script.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+START=$(date +%s)
+user='ylony'
+version='0.1 - 25-12-18'
+gitName="Dimitri BERGER"
+gitMail="hylow423@gmail.com"
+
+echo "User selectionné : $user, la version du script est : $version"
+
+# zsh git lib-hyper
+
+echo "Installation de Git, Zsh et des libs pour le terminal Hyper"
+
+sudo apt-get update > /dev/null
+sudo apt-get install -y git zsh libdbusmenu-gtk4 libappindicator1 libindicator7 > /dev/null
+
+# Firefox
+
+echo "Téléchargement de Firefox"
+
+wget https://download-installer.cdn.mozilla.net/pub/devedition/releases/65.0b6/linux-x86_64/fr/firefox-65.0b6.tar.bz2 -q --show-progress
+
+echo "Extraction ..."
+
+sudo tar -xf firefox-65.0b6.tar.bz2 
+
+sudo mkdir /opt
+
+sudo mv ./firefox /opt/
+
+sudo echo "alias firefox='/opt/firefox/firefox 2> /dev/null &'" >> /home/$user/.bashrc
+sudo echo "alias firefox='/opt/firefox/firefox 2> /dev/null &'" >> /home/$user/.zshrc
+
+# Oh my zsh
+echo "Installation de Oh-my-zsh"
+
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Hyper Terminal
+
+echo "Téléchargement du terminal Hyper"
+
+wget 'https://github-production-release-asset-2e65be.s3.amazonaws.com/62367558/8b05cf00-0078-11e9-8b9c-3b6266436298?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20181225%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20181225T105017Z&X-Amz-Expires=300&X-Amz-Signature=8853ee88046a749a793199d8e6686d8e3a536a650d9b87cf6ef81439834ffc6b&X-Amz-SignedHeaders=host&actor_id=0&response-content-disposition=attachment%3B%20filename%3Dhyper_2.1.0_amd64.deb&response-content-type=application%2Foctet-stream' -q --show-progress
+
+sudo dpkg -i ./hyper_2.1.0_amd64.deb > /dev/null
+
+echo "Installation du theme zsh Pure"
+git clone https://github.com/sindresorhus/pure > /dev/null
+sudo ln -s "$PWD/pure/pure.zsh" /usr/local/share/zsh/site-functions/prompt_pure_setup
+sudo ln -s "$PWD/pure/async.zsh" /usr/local/share/zsh/site-functions/async
+
+sudo echo 'autoload -U promptinit; promptinit' >> /home/$user/.zshrc
+sudo echo 'prompt pure' >> /home/$user/.zshrc
+
+echo "Installation de l'autocomplete pour zsh"
+
+sudo mkdir /home/$user/.zsh/
+
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions /home/$user/.zsh/zsh-autosuggestions > /dev/null
+
+sudo echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' >> /home/$user/.zshrc
+
+echo "Installation du theme hyper-snazzy pour hyper"
+
+hyper install hyper-snazzy >/dev/null
+
+echo "Installation du syntax highlighter pour zsh"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git > /dev/null
+
+echo "source $(pwd)/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> /home/$user/.zshrc
+
+echo "Configuration de git :"
+
+git config --global user.email="$gitMail"
+git config --global user.name="$gitName"
+
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+
+echo "Fin après $DIFF secondes"
+
