126da5061c0cacdf11e74c140c284848c11e06e7
Dimitri BERGER <hylow423@gmail.com>
2018-12-25 13:04:09 +0100

    docker & docker-compose

--- script.sh
+++ script.sh
@@ -92,6 +92,23 @@ sudo dpkg -i code_1.30.1-1545156774_amd64.deb > /dev/null
 echo "Installation de Vim"
 sudo apt-get install -y vim > /dev/null
 
+echo "Installation de Docker & Docker-Compose"
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common > /dev/null
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update > /dev/null
+sudo apt-get install -y docker-ce > /dev/null
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 END=$(date +%s)
 DIFF=$(( $END - $START ))
 
